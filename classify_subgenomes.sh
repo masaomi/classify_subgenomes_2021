9cbb247f28ffa6cf52aba30041f5be96151bdf32
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2021-12-27 13:37:15 +0100

    Fixed classify_subgenomes.sh

--- classify_subgenomes.sh
+++ classify_subgenomes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # encoding: utf-8
-# Version = '20211227-110309'
+# Version = '20211227-133704'
 
 # required
 # * references/
@@ -32,7 +32,7 @@ ruby scripts/make_depthave_length.rb samtools_depth_out references/2Es_HS_3.2.1_
 ruby scripts/make_homeolog_list.rb references/2Es_HS_3.2.1_maker.gff gene_depth_length.dat homeolog_candidates_list.dat
 # homeolog_list.dat generated
 
-ruby scripts/scaff_ab_homeolog_count_ratio.rb references/2Es_HS_3.2.1_genome.fa homeolog_list.dat --a_homeolog_ratio 0.5 2> /dev/null
+ruby scripts/scaff_ab_homeolog_count_ratio.rb references/2Es_HS_3.2.1_genome.fa references/2Es_HS_3.2.1_genes.gff homeolog_list.dat --a_homeolog_ratio 0.5 2> /dev/null
 # scaff_ab_homeolog_count_ratio.dat generated
 
 ruby scripts/make_AB_annotation_based_on_homeolog_count_ratio.rb references/2Es_HS_3.2.1_genome.fa references/2Es_HS_3.2.1_maker.gff scaff_ab_homeolog_count_ratio.dat homeolog_list.dat --homeolog_ratio_threshold 0.6 --out_dir 2Es_classify_out_homeolog_ratio_0.6

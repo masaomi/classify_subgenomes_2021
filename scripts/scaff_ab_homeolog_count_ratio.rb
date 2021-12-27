0e7f725087de7dbb5cec62c0af9c728861c33355
Masaomi Hatakeyama <masaomi.hatakeyama@gmail.com>
2021-12-27 13:34:09 +0100

    Fixed scaff_ab_homeolog_count_ratio.rb

--- scripts/scaff_ab_homeolog_count_ratio.rb
+++ scripts/scaff_ab_homeolog_count_ratio.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-# Version = '20190122-145435'
+# Version = '20211227-133053'
 
 require 'csv'
 
 A_HOMEOLOG_RATIO = 0.5
 
-unless genome_fa=ARGV[0] and homeolog_list_dat=ARGV[1]
+unless genome_fa=ARGV[0] and genes_gff=ARGV[1] and homeolog_list_dat=ARGV[2]
   puts <<-eos
   usage:
-    #{File.basename(__FILE__)} [genome.fa] [homeolog_list.dat]
+    #{File.basename(__FILE__)} [genome.fa] [genes.gff] [homeolog_list.dat]
   note:
     you can skip nil case by 2> /dev/null
   eos
@@ -23,15 +23,27 @@ Bio::FlatFile.open(genome_fa).each do |e|
   sid = e.definition.gsub('|', '_').gsub('_obj', '')
   scaff2len[sid] = e.seq.length
 end
-#pp scaff2len
+#p scaff2len
 #exit
+
+gid2sid = {}
+File.readlines(genes_gff).each do |line|
+  unless line =~ /^#/
+    sid, maker, type, *others = line.split
+    if type == "gene" and line =~ /ID=(.+?);/
+      gid = $1
+      gid2sid[gid] = sid
+    end
+  end
+end
+
 scaff_a_count = {}
 scaff_b_count = {}
 CSV.foreach(homeolog_list_dat, :headers=>true, :col_sep=>"\t") do |row|
   a_gene = row["A_homeolog"]
   b_gene = row["B_homeolog"]
-  scaff_a_gene = a_gene.gsub(/maker-/, '').gsub(/augustus_masked-/, '').gsub(/_obj.+/, '').gsub(/-augustus-gene-.+/, '').gsub(/-processed-gene-.+/, '').gsub(/-exonerate_.+/, '').gsub(/-est_.+/, '')
-  scaff_b_gene = b_gene.gsub(/maker-/, '').gsub(/augustus_masked-/, '').gsub(/_obj.+/, '').gsub(/-augustus-gene-.+/, '').gsub(/-processed-gene-.+/, '').gsub(/-exonerate_.+/, '').gsub(/-est_.+/, '')
+  scaff_a_gene = gid2sid[a_gene]
+  scaff_b_gene = gid2sid[b_gene]
   scaff_a_count[scaff_a_gene] ||= 0
   scaff_a_count[scaff_a_gene] += 1
   scaff_b_count[scaff_b_gene] ||= 0
@@ -40,8 +52,9 @@ end
 
 all_scaff = scaff_a_count.keys.concat(scaff_b_count.keys)
 all_scaff = all_scaff.uniq.sort
-#pp all_scaff
+#p all_scaff
 #exit
+
 sid2ab = {}
 open("scaff_ab_homeolog_count_ratio.dat", "w") do |out|
   out.puts ["scaffold", "A_gene_count", "B_gene_count", "A_homeolog_ratio", "scaff_len"].join("\t")
